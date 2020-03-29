f6fad5f886b506c10943b421cdb30d23c0034709
Jeff Williams <jwill1258@gmail.com>
2020-03-29 14:17:45 -0400

    Routes

---
+++ client/build/precache-manifest.a5e7b1fb60699b6a7164cc028e8881b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76196db5e8c1dea295ee",
+    "url": "/static/css/main.259a0e98.chunk.css"
+  },
+  {
+    "revision": "76196db5e8c1dea295ee",
+    "url": "/static/js/main.47d6d760.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f14e103a85ded4a2db8",
+    "url": "/static/js/2.9f08725b.chunk.js"
+  },
+  {
+    "revision": "f0d3d29fef2e48f3d9860498ef09e313",
+    "url": "/index.html"
+  }
+];
